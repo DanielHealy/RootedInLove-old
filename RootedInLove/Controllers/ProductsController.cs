25468c35d7b1edb7adcb5cce65f499922caf222d
Daniel Healy <daniel.thomas.healy@gmail.com>
2017-03-07 12:35:45 -0600

    Edited details action in product controller. Cleaned up other parts of productscontroller

--- RootedInLove/Controllers/ProductsController.cs
+++ RootedInLove/Controllers/ProductsController.cs
@@ -14,126 +14,42 @@ namespace RootedInLove.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Products
+        #region Index (Display all Materials)
         public ActionResult Index()
         {
             ViewBag.Title = "All Materials";
             return View(db.Products.ToList());
         }
+        #endregion
 
-        // GET: Products/Details/5
+        #region Author (Show all materials by the author in URL)
         public ActionResult Author(string id="James")
         {
-            var authors =
-                from people in db.Artists
+            var author =
+                (from people in db.Artists
                 where people.ShortName == id
-                select people;
-            if (authors.Count()==0)
-            {
-                return HttpNotFound();
-            }
-            var authorID = authors.First().ID;
-            var authorName = authors.First().FullName;
+                select people).First();
 
             var product = 
                 from item in db.Products
-                where item.Author.ID == authorID
+                where item.Author.ID == author.ID
                 select item;
-            if (product.Count()==0)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Title = "Materials from "+authorName;
+
+            ViewBag.Title = "Materials from "+author.FullName;
             return View("Index",product.ToList());
         }
+        #endregion
 
-        // GET: Products/Create
-        public ActionResult Create()
+        #region Details (Show details for material in URL)
+        public ActionResult Details(string id="Cake")
         {
-            return View();
+            var material =
+                (from items in db.Products
+                 where items.ShortName == id
+                 select items).First();
+          
+            return View(material);
         }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,Description,Image,AddToCartLink,GooglePlayLink,ShortName")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(product);
-        }
-
-        // GET: Products/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,Description,Image,AddToCartLink,GooglePlayLink,ShortName")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(product);
-        }
-
-        // GET: Products/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Product product = db.Products.Find(id);
-            db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        #endregion
     }
 }
