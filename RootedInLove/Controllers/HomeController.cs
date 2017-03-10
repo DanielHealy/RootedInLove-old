4abe7d4ad5153bf73eaf660422da011d06fe0e7e
Daniel Healy <daniel.thomas.healy@gmail.com>
2017-03-10 16:23:57 -0600

    Calibrated About page

--- RootedInLove/Controllers/HomeController.cs
+++ RootedInLove/Controllers/HomeController.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using RootedInLove.Models;
 
 namespace RootedInLove.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(string id = "James")
         {
-            ViewBag.Message = "Your application description page.";
+            var author =
+                (from people in db.Artists
+                 where people.ShortName == id
+                 select people).First();
 
-            return View();
+            return View(author);
         }
 
         public ActionResult Contact()
